9270d2ad0f0e003652ff99d7ce27fb82ad5ca3f7
Kirill <pesterevkv2003@gmail.com>
2025-02-02 11:18:04 +0300

    added cors, prisma studio in docker-compose

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const fs = require("fs");
-require('dotenv').config()
+require("dotenv").config();
+var cors = require("cors");
 
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
